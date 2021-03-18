a07a2b557f0c9aaa1899b8023525d57ad108a917
A-Prudnikova <a.prudnikova@gmail.com>
2021-03-18 19:31:19 +0300

    squashed commits

--- src/test/java/tests/SelenideGithubPageTest.java
+++ src/test/java/tests/SelenideGithubPageTest.java
@@ -1,24 +1,17 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class selenideGithubPageTest {
+public class SelenideGithubPageTest {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
-
-
     }
 
     @Test
@@ -27,7 +20,6 @@ public class selenideGithubPageTest {
         actions().sendKeys("g", "w").perform();
         $(byText("SoftAssertions")).click();
         $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
-
     }
 }
 
