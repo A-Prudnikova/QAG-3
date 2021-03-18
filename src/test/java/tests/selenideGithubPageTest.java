88661fd5c0cfc167ce8a35fc44d5f2c076f06fe3
A-Prudnikova <a.prudnikova@gmail.com>
2021-03-18 18:02:07 +0300

    test-1

---
+++ src/test/java/tests/selenideGithubPageTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class selenideGithubPageTest {
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+
+
+    }
+
+    @Test
+    void softAssertions() {
+        open("https://github.com/selenide/selenide");
+        actions().sendKeys("g", "w").perform();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+
+    }
+}
+
